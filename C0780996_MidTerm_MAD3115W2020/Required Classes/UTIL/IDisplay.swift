f1e0c78f6960da49dbcabc212acaf62fc8bb9ca3
raghavbobal1 <raghavbobal001@gmail.com>
2020-03-05 03:39:19 -0500

    [Raghav Bobal] [P1] Created iDisplaydelegate protocol

---
+++ C0780996_MidTerm_MAD3115W2020/Required Classes/UTIL/IDisplay.swift
@@ -0,0 +1,14 @@
+//
+//  IDisplayDelegate.swift
+//  C0780996_MidTerm_MAD3115W2020
+//
+//  Created by Raghav Bobal on 2020-03-05.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+
+protocol IDisplayDelegate
+{
+    func display()
+}
