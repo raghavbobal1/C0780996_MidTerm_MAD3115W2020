a846e40d2b5dfdc1210e0b9170476de0783de562
raghavbobal1 <raghavbobal001@gmail.com>
2020-03-06 21:40:53 -0500

    [Raghav Bobal] [P1] created the view did load func and added an alert controller if the email/ password is blank

--- C0780996_MidTerm_MAD3115W2020/LoginViewController.swift
+++ C0780996_MidTerm_MAD3115W2020/LoginViewController.swift
@@ -13,12 +13,35 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTxt: UITextField!
     @IBOutlet weak var passwordTxt: UITextField!
     @IBOutlet weak var remMeSwitch: UISwitch!
+   
     
     override func viewDidLoad()
+       {
+            super.viewDidLoad()
+            let userDf = UserDefaults.standard
+            let name = userDf.string(forKey:"name")
+            let pswrd = userDf.string(forKey: "password")
+            self.navigationItem.hidesBackButton = true
+                if let uName = name
+                {
+                    emailTxt.text = uName
+                }
+                if let password = pswrd
+                {
+                    passwordTxt.text=password
+                }
+       }
+    @IBAction func btnLogin(_ sender: Any)
     {
-        super.viewDidLoad()
-    
+        if (emailTxt.text == "" || passwordTxt.text == "")
+          {
+        let alertController = UIAlertController(title: "Error", message: "Email/Password is blank", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+            self.present(alertController, animated: true, completion: nil)
+          }
+        
     }
+   
 
 
 }
