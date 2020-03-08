d533a77f77b1a6ab0948e5c632567a126b40cf52
raghavbobal1 <raghavbobal001@gmail.com>
2020-03-07 21:32:28 -0500

    [Raghav Bobal] [P1] made connections in the addnewcustomer view control to the addnew customer class

--- C0780996_MidTerm_MAD3115W2020/ViewController/AddNewCustomerViewController.swift
+++ C0780996_MidTerm_MAD3115W2020/ViewController/AddNewCustomerViewController.swift
@@ -8,23 +8,30 @@
 
 import UIKit
 
-class AddNewCustomerViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class AddNewCustomerViewController: UIViewController
+{
+    @IBOutlet weak var lblAddCust: UILabel!
+    @IBOutlet weak var newCustomerIDtxt: UITextField!
+    @IBOutlet weak var newCustomerNametxt: UITextField!
+    @IBOutlet weak var newCustomerEmailtxt: UITextField!
+    @IBOutlet weak var newCustomerPasswordtxt: UITextField!
+    @IBOutlet weak var newCustomerMobileNumbertxt: UITextField!
+    
+    @IBAction func btnAddCustomer(_ sender: Any)
+    {
+        let id = newCustomerIDtxt.text!
+        let name = newCustomerNametxt.text!
+        let number = newCustomerMobileNumbertxt.text!
+        let email  = newCustomerEmailtxt.text!
+        let password = newCustomerPasswordtxt.text!
+        let mobileNumber = newCustomerMobileNumbertxt.text!
+        
     }
     
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
+    
 }
