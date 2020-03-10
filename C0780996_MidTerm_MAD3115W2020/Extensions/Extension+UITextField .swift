50f26741b7cd25a785266bedbc90f91354527989
raghavbobal1 <raghavbobal001@gmail.com>
2020-03-10 19:28:28 -0400

    [Raghav Bobal][P2] Added an extension for date picker view

---
+++ C0780996_MidTerm_MAD3115W2020/Extensions/Extension+UITextField .swift
@@ -0,0 +1,33 @@
+//
+//  Extension+UITextField .swift
+//  C0780996_MidTerm_MAD3115W2020
+//
+//  Created by Raghav Bobal on 2020-03-10.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UITextField
+{
+    func setInputViewDatePicker(target: Any, selector: Selector)
+    {
+        let screenWidth = UIScreen.main.bounds.width
+        let datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 216))
+        datePicker.datePickerMode = .date
+        self.inputView = datePicker
+        
+        let toolBar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: screenWidth, height: 44.0))
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancel = UIBarButtonItem(title: "Cancel", style: .plain, target: nil, action: #selector(tapCancel))
+        let barButton = UIBarButtonItem(title: "Done", style: .plain, target: target, action: selector)
+        toolBar.setItems([cancel, flexible, barButton], animated: false)
+        self.inputAccessoryView = toolBar
+    }
+    
+    @objc func tapCancel()
+    {
+        self.resignFirstResponder()
+    }
+    
+}
