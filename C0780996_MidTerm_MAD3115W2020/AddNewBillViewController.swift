62cfcb91574e898771aea2de722cc218b253973e
raghavbobal1 <raghavbobal001@gmail.com>
2020-03-13 19:50:43 -0400

    [Raghav Bobal] [P2] Final Commit. everything works

--- C0780996_MidTerm_MAD3115W2020/AddNewBillViewController.swift
+++ C0780996_MidTerm_MAD3115W2020/AddNewBillViewController.swift
@@ -5,17 +5,16 @@ class AddNewBillViewController: UIViewController
 {
     @IBOutlet weak var txtBillId: UITextField!
     @IBOutlet weak var txtBillDate: UITextField!
-    @IBOutlet weak var txtBillType: UITextField!
     @IBOutlet weak var segmentBilltype: UISegmentedControl!
     @IBOutlet weak var txtHydUnits: UITextField!
-    @IBOutlet weak var txtHydAgency: UITextField!
+    @IBOutlet weak var txtHydProvider: UITextField!
     @IBOutlet weak var txtIntData: UITextField!
     @IBOutlet weak var txtIntProvider: UITextField!
     @IBOutlet weak var txtMobMins: UITextField!
     @IBOutlet weak var txtMobData: UITextField!
     @IBOutlet weak var txtMobNumber: UITextField!
     @IBOutlet weak var txtMobPlan: UITextField!
-    @IBOutlet weak var txtMobManufacturer: UITextField!
+    @IBOutlet weak var txtMobProvider: UITextField!
     var selectedCustomer: Customer?
     var currentSlected: Int = 0
     var datePicker : UIDatePicker!
@@ -38,64 +37,77 @@ class AddNewBillViewController: UIViewController
         self.txtBillDate.resignFirstResponder()
     }
     
+    func billSuccess()
+    {
+        let alertController = UIAlertController(title: "Success", message: "Bill Added", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in self.dismissView()}))
+        self.present(alertController, animated: true, completion: nil)
+    }
     
-    @IBAction func btnAddBill(_ sender: Any) {
-        if (segmentBilltype.selectedSegmentIndex == 0){
-            if(Validations.mobileNumber(number: txtMobNumber.text!) == true){
-                if((txtBillId.text?.contains("mob"))!)
-                {
-                    let billObj = Mobile(billId: txtBillId.text!, billDate:  (txtBillDate.text?.toDate())!, billType: BillType.Mobile
-                    ,mobileManufacturerName: txtMobManufacturer.text!, planName: txtMobPlan!.text!, mobileNumber: txtMobNumber!.text!, mobileDataUsed: Double(txtMobData!.text!)!, callMinutesUsed: Int(txtMobMins!.text!)!)
+    
+    @IBAction func btnAddBill(_ sender: Any)
+    {
+        if (segmentBilltype.selectedSegmentIndex == 1)
+        {
+            if((txtBillId.text?.contains("hy"))!)
+            {
+                let billObj = Hydro(billId: txtBillId.text!, billDate:  (txtBillDate.text?.toDate())!, billType: BillType.Hydro, agencyName: txtHydProvider!.text!, unitsConsumed: Double(txtHydUnits!.text!)!)
                 selectedCustomer?.newBill(bill: billObj, billId: txtBillId.text!)
-                    billSuccess()
-                }
-                else{
-                    let alertController = UIAlertController(title:"Error", message: "Bill ID must contain MB", preferredStyle: .alert)
-                    alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
-                    self.present(alertController, animated: true, completion: nil)
-                    return
-                }
+                billSuccess()
             }
-            let alertController = UIAlertController(title:"Error", message: "Invalid phone number", preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
-            self.present(alertController, animated: true, completion: nil)
-            return
-        }
-        if(segmentBilltype.selectedSegmentIndex == 1){
-            if((txtBillId.text?.contains("hy"))!){
-                let billObj = Hydro(billId: txtBillId.text!, billDate:  (txtBillDate.text?.toDate())!, billType: BillType.Hydro, agencyName: txtHydAgency!.text!, unitsConsumed: Double(txtHydUnits!.text!)!)
-            selectedCustomer?.newBill(bill: billObj, billId: txtBillId.text!)
-            billSuccess()
-            }
-            else {
-                let alertController = UIAlertController(title:"Error", message: "Bill ID must contain HY", preferredStyle: .alert)
+            else
+            {
+                let alertController = UIAlertController(title:"Error", message: "Bill ID must contain hy", preferredStyle: .alert)
                 alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
                 self.present(alertController, animated: true, completion: nil)
                 return
             }
         }
-        if(segmentBilltype.selectedSegmentIndex == 2){
-            if((txtBillId.text?.contains("in"))!){
+        if(segmentBilltype.selectedSegmentIndex == 3)
+        {
+            if((txtBillId.text?.contains("in"))!)
+            {
                 let billObj = Internet(billId: txtBillId.text!, billDate:  (txtBillDate.text?.toDate())!, billType: BillType.Internet, providerName: txtIntProvider!.text!, internetGbUsed: Double(txtIntData!.text!) as! Double)
-            selectedCustomer?.newBill(bill: billObj, billId: txtBillId.text!)
-            billSuccess()
+               
+                selectedCustomer?.newBill(bill: billObj, billId: txtBillId.text!)
+                
+                billSuccess()
             }
-            else {
+            else
+            {
                 let alertController = UIAlertController(title:"Error", message: "Bill ID must contain in", preferredStyle: .alert)
                 alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
                 self.present(alertController, animated: true, completion: nil)
                 return
             }
         }
+        if(segmentBilltype.selectedSegmentIndex == 0)
+        {
+            if(Validations.mobileNumber(number: txtMobNumber.text!) == true)
+            {
+              if((txtBillId.text?.contains("mob"))!)
+              {
+                let billObj = Mobile(billId: txtBillId.text!, billDate: (txtBillDate.text?.toDate())!, billType: BillType.Mobile,mobileManufacturerName: txtMobProvider.text!, planName: txtMobPlan!.text!, mobileNumber: txtMobNumber!.text!, mobileDataUsed: Double(txtMobData!.text!)!,callMinutesUsed: Int(txtMobMins!.text!)!)
+                selectedCustomer?.newBill(bill: billObj, billId: txtBillId.text!)
+                billSuccess()
+              }
+                else
+                {
+                  let alertController = UIAlertController(title:"Error", message: "Bill ID must contain mob", preferredStyle: .alert)
+                  alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+                  self.present(alertController, animated: true, completion: nil)
+                  return
+                }
+            }
+              let alertController = UIAlertController(title:"Error", message: "Invalid phone number", preferredStyle: .alert)
+              alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+            self.present(alertController, animated: true, completion: nil)
+             return
+        }
     }
-    func billSuccess(){
-        let alertController = UIAlertController(title: "Success", message: "Bill Added", preferredStyle: .alert)
-        //alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in self.forceReloadBillTable()}))
-        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in self.dismissView()}))
-        self.present(alertController, animated: true, completion: nil)
-    }
-    
-    func forceReloadBillTable(){
+        
+    func forceReloadBillTable()
+    {
         let sb = UIStoryboard(name: "Main", bundle: nil)
         let showBillDetailsVC = sb.instantiateViewController(identifier: "showBillDetailsVC") as! ShowBillDetailsViewController
         navigationController?.pushViewController(showBillDetailsVC, animated: true)
@@ -107,49 +119,54 @@ class AddNewBillViewController: UIViewController
         navigationController?.popViewController(animated: true)
     }
     
-    func firstSetting(){
+    func firstSetting()
+    {
     self.txtMobMins.isHidden = true
     self.txtMobData.isHidden = true
-    self.txtMobManufacturer.isHidden = true
+    self.txtMobProvider.isHidden = true
     self.txtMobPlan.isHidden = true
     self.txtMobNumber.isHidden = true
-    self.txtMobManufacturer.isHidden = true
+    self.txtMobProvider.isHidden = true
     }
-    func initialSetting(){
+    func initialSetting()
+    {
         self.txtHydUnits.isHidden = true
-        self.txtHydAgency.isHidden = true
+        self.txtHydProvider.isHidden = true
         self.txtIntData.isHidden = true
         self.txtIntProvider.isHidden = true
         self.txtMobMins.isHidden = false
         self.txtMobData.isHidden = false
-        self.txtMobManufacturer.isHidden = false
+        self.txtMobProvider.isHidden = false
         self.txtMobPlan.isHidden = false
         self.txtMobNumber.isHidden = false
-        self.txtMobManufacturer.isHidden = false
+        self.txtMobProvider.isHidden = false
     }
     
     
-    @IBAction func segBill(_ sender: Any) {
-        if (segmentBilltype.selectedSegmentIndex == 0) {
+    @IBAction func segmentType(_ sender: Any)
+    {
+        if (segmentBilltype.selectedSegmentIndex == 0)
+        {
             self.txtHydUnits.isHidden = true;
-            self.txtHydAgency.isHidden = true;
+            self.txtHydProvider.isHidden = true;
             initialSetting()
         }
-        if(segmentBilltype.selectedSegmentIndex == 1) {
+        if(segmentBilltype.selectedSegmentIndex == 1)
+        {
             firstSetting()
-            self.txtHydAgency.isHidden = false
+            self.txtHydProvider.isHidden = false
             self.txtHydUnits.isHidden = false
             self.txtIntData.isHidden = true
             self.txtIntProvider.isHidden = true
         }
-        if(segmentBilltype.selectedSegmentIndex == 2){
+        if(segmentBilltype.selectedSegmentIndex == 2)
+        {
             firstSetting()
-            self.txtHydAgency.isHidden = true
+            self.txtHydProvider.isHidden = true
             self.txtHydUnits.isHidden = true
             self.txtIntData.isHidden = false
             self.txtIntProvider.isHidden = false
 
         }
     }
-    
 }
