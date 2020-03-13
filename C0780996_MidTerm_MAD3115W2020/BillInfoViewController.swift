aed330d4c5ada71c9877fbe438930f6b69373b24
raghavbobal1 <raghavbobal001@gmail.com>
2020-03-13 05:36:47 -0400

    [Raghav Bobal] [P2] connected 2 labels from billing vc and added size, font and colours for labels

--- C0780996_MidTerm_MAD3115W2020/BillInfoViewController.swift
+++ C0780996_MidTerm_MAD3115W2020/BillInfoViewController.swift
@@ -4,10 +4,22 @@ import UIKit
 
 class BillInfoViewController: UIViewController
 {
-
+    var customerBill: Customer?
+    var bills = [Bill]()
+    
+    @IBOutlet weak var lblBckGrnd: UILabel!
+    @IBOutlet weak var lblTotalAmount: UILabel!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        self.bills = customerBill!.getBills()
+        self.title = "CUSTOMER BILLS"
+        self.lblTotalAmount.text = String(format:"$%.2f", (customerBill?.billTotal())!)
+        lblTotalAmount.font = lblTotalAmount.font.withSize(40.0)
+        lblTotalAmount.textColor = UIColor.white
+        lblTotalAmount.textAlignment = .center
+        lblBckGrnd.backgroundColor = UIColor.systemBlue
     }
 
 }
