036f40e7940286c9d5c9b50f5195a688182ce496
raghavbobal1 <raghavbobal001@gmail.com>
2020-03-13 19:56:45 -0400

    [Raghav Bobal] [P2] code not pushing

--- C0780996_MidTerm_MAD3115W2020/Extensions/Extension+String.swift
+++ C0780996_MidTerm_MAD3115W2020/Extensions/Extension+String.swift
@@ -10,7 +10,8 @@ import Foundation
 
 extension String
 {
-  func toDate(withFormat format: String = "dd/MM/yyyy") -> Date {
+  func toDate(withFormat format: String = "MMM dd, yyyy") -> Date
+  {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = format
     guard let date = dateFormatter.date(from: self) else {
