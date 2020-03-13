f0abd34a97885ffafbee55194153a7baa19c899b
raghavbobal1 <raghavbobal001@gmail.com>
2020-03-13 07:47:18 -0400

    [Raghav Bobal][P2] connected variables  and functions in billinfovc and add new vc

--- C0780996_MidTerm_MAD3115W2020/AddNewBillViewController.swift
+++ C0780996_MidTerm_MAD3115W2020/AddNewBillViewController.swift
@@ -3,12 +3,153 @@ import UIKit
 
 class AddNewBillViewController: UIViewController
 {
-    var selectedCustomer : Customer?
+    @IBOutlet weak var txtBillId: UITextField!
+    @IBOutlet weak var txtBillDate: UITextField!
+    @IBOutlet weak var txtBillType: UITextField!
+    @IBOutlet weak var segmentBilltype: UISegmentedControl!
+    @IBOutlet weak var txtHydUnits: UITextField!
+    @IBOutlet weak var txtHydAgency: UITextField!
+    @IBOutlet weak var txtIntData: UITextField!
+    @IBOutlet weak var txtIntProvider: UITextField!
+    @IBOutlet weak var txtMobMins: UITextField!
+    @IBOutlet weak var txtMobData: UITextField!
+    @IBOutlet weak var txtMobNumber: UITextField!
+    @IBOutlet weak var txtMobPlan: UITextField!
+    @IBOutlet weak var txtMobManufacturer: UITextField!
+    var selectedCustomer: Customer?
+    var currentSlected: Int = 0
+    var datePicker : UIDatePicker!
+    var myPickerView : UIPickerView!
+    var pickerDataBillType = ["Mobile" , "Internet" , "Hydro"]
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
+        self.navigationItem.hidesBackButton = true
+        self.txtBillDate.setInputViewDatePicker(target: self, selector: #selector(tapDone))
+        self.initialSetting()
+    
+    }
+    @objc func tapDone() {
+        if let datePicker = self.txtBillDate.inputView as? UIDatePicker {
+            let dateformatter = DateFormatter()
+            dateformatter.dateStyle = .medium
+            self.txtBillDate.text = dateformatter.string(from: datePicker.date)
+        }
+        self.txtBillDate.resignFirstResponder()
     }
     
+    
+    @IBAction func btnAddBill(_ sender: Any) {
+        if (segmentBilltype.selectedSegmentIndex == 0){
+            if(Validations.mobileNumber(number: txtMobNumber.text!) == true){
+                if((txtBillId.text?.contains("MB"))!)
+                {
+                    let billObj = Mobile(billId: txtBillId.text!, billDate:  (txtBillDate.text?.toDate())!, billType: BillType.Mobile
+                    ,mobileManufacturerName: txtMobManufacturer.text!, planName: txtMobPlan!.text!, mobileNumber: txtMobNumber!.text!, mobileDataUsed: Double(txtMobData!.text!)!, callMinutesUsed: Int(txtMobMins!.text!)!)
+                selectedCustomer?.newBill(bill: billObj, billId: txtBillId.text!)
+                    billSuccess()
+                }
+                else{
+                    let alertController = UIAlertController(title:"Error", message: "Bill ID must contain MB", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+                    self.present(alertController, animated: true, completion: nil)
+                    return
+                }
+            }
+            let alertController = UIAlertController(title:"Error", message: "Invalid phone number", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+            self.present(alertController, animated: true, completion: nil)
+            return
+        }
+        if(segmentBilltype.selectedSegmentIndex == 1){
+            if((txtBillId.text?.contains("HY"))!){
+                let billObj = Hydro(billId: txtBillId.text!, billDate:  (txtBillDate.text?.toDate())!, billType: BillType.Hydro, agencyName: txtHydAgency!.text!, unitsConsumed: Double(txtHydUnits!.text!)!)
+            selectedCustomer?.newBill(bill: billObj, billId: txtBillId.text!)
+            billSuccess()
+            }
+            else {
+                let alertController = UIAlertController(title:"Error", message: "Bill ID must contain HY", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+        }
+        if(segmentBilltype.selectedSegmentIndex == 2){
+            if((txtBillId.text?.contains("IN"))!){
+                let billObj = Internet(billId: txtBillId.text!, billDate:  (txtBillDate.text?.toDate())!, billType: BillType.Internet, providerName: txtIntProvider!.text!, internetGbUsed: Double(txtIntData!.text!) as! Double)
+            selectedCustomer?.newBill(bill: billObj, billId: txtBillId.text!)
+            billSuccess()
+            }
+            else {
+                let alertController = UIAlertController(title:"Error", message: "Bill ID must contain IN", preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+        }
+    }
+    func billSuccess(){
+        let alertController = UIAlertController(title: "Success", message: "Bill Added", preferredStyle: .alert)
+        //alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in self.forceReloadBillTable()}))
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in self.dismissView()}))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func forceReloadBillTable(){
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let showBillDetailsVC = sb.instantiateViewController(identifier: "showBillDetailsVC") as! ShowBillDetailsViewController
+        navigationController?.pushViewController(showBillDetailsVC, animated: true)
+    }
+    
+    func dismissView()
+    {
 
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func firstSetting(){
+    self.txtMobMins.isHidden = true
+    self.txtMobData.isHidden = true
+    self.txtMobManufacturer.isHidden = true
+    self.txtMobPlan.isHidden = true
+    self.txtMobNumber.isHidden = true
+    self.txtMobManufacturer.isHidden = true
+    }
+    func initialSetting(){
+        self.txtHydUnits.isHidden = true;
+        self.txtHydAgency.isHidden = true;
+        self.txtIntData.isHidden = true
+        self.txtIntProvider.isHidden = true
+        self.txtMobMins.isHidden = false
+        self.txtMobData.isHidden = false
+        self.txtMobManufacturer.isHidden = false
+        self.txtMobPlan.isHidden = false
+        self.txtMobNumber.isHidden = false
+        self.txtMobManufacturer.isHidden = false
+    }
+    
+    
+    @IBAction func segBill(_ sender: Any) {
+        if (segmentBilltype.selectedSegmentIndex == 0) {
+            self.txtHydUnits.isHidden = true;
+            self.txtHydAgency.isHidden = true;
+            initialSetting()
+        }
+        if(segmentBilltype.selectedSegmentIndex == 1) {
+            firstSetting()
+            self.txtHydAgency.isHidden = false
+            self.txtHydUnits.isHidden = false
+            self.txtIntData.isHidden = true
+            self.txtIntProvider.isHidden = true
+        }
+        if(segmentBilltype.selectedSegmentIndex == 2){
+            firstSetting()
+            self.txtHydAgency.isHidden = true
+            self.txtHydUnits.isHidden = true
+            self.txtIntData.isHidden = false
+            self.txtIntProvider.isHidden = false
+
+        }
+    }
+    
 }
--- C0780996_MidTerm_MAD3115W2020/BillInfoViewController.swift
+++ C0780996_MidTerm_MAD3115W2020/BillInfoViewController.swift
@@ -7,19 +7,20 @@ class BillInfoViewController: UIViewController
     var customerBill: Customer?
     var bills = [Bill]()
     
+    @IBOutlet weak var tblBillDetails: UITableView!
     @IBOutlet weak var lblBckGrnd: UILabel!
     @IBOutlet weak var lblTotalAmount: UILabel!
     
     override func viewDidLoad()
     {
-        super.viewDidLoad()
+        
         self.bills = customerBill!.getBills()
-        self.title = "CUSTOMER BILLS"
         self.lblTotalAmount.text = String(format:"$%.2f", (customerBill?.billTotal())!)
-        lblTotalAmount.font = lblTotalAmount.font.withSize(40.0)
+        lblTotalAmount.font = lblTotalAmount.font.withSize(30.0)
         lblTotalAmount.textColor = UIColor.white
         lblTotalAmount.textAlignment = .center
-        lblBckGrnd.backgroundColor = UIColor.systemBlue
+        self.addNewBillButton()
+        lblBckGrnd.backgroundColor = UIColor .systemBlue
     }
     
     private func addNewBillButton()
@@ -63,37 +64,37 @@ extension BillInfoViewController: UITableViewDataSource,
         let cell = tableView.dequeueReusableCell(withIdentifier: "BillTableViewCell") as! BillTableViewCell
         let billList = bills[indexPath.row]
         
-        if billList.billId.contains("MB")
+        if billList.billId.contains("mob")
         {
-          cell.lblbillId?.text = billList.billId
-          cell.lblbillDate?.text = billList.billDate.getFormattedDate()
-          cell.imgBillicon.image = UIImage(named:"mobileicon")
-          cell.billAmount.text = String(format:"$%.2f", billList.billCalculate())
-          cell.backgroundColor = UIColor(displayP3Red: 0.98, green: 0.91, blue: 0.71, alpha: 1.0)
+          cell.lblBillId?.text = billList.billId
+          cell.lblBillDate?.text = billList.billDate.getFormattedDate()
+          cell.imgIcon.image = UIImage(named:"mobileIcon")
+          cell.lblBillAmount.text = String(format:"$%.2f", billList.billCalculate())
+          cell.backgroundColor = UIColor(red:0.97, green:0.84, blue:1.00, alpha:1.0)
         }
            
-        if billList.billId.contains("HY")
+        if billList.billId.contains("hy")
         {
-         cell.lblbillId?.text = billList.billId
-         cell.lblbillDate?.text = billList.billDate.getFormattedDate()
-         cell.imgBillicon.image = UIImage(named:"hydroicon")
-         cell.billAmount.text = String(format:"$%.2f", billList.billCalculate())
-         cell.backgroundColor = UIColor(displayP3Red: 0.91, green: 1.0, blue: 1.0, alpha: 1.0)
+         cell.lblBillId?.text = billList.billId
+         cell.lblBillDate?.text = billList.billDate.getFormattedDate()
+         cell.imgIcon.image = UIImage(named:"hydroIcon")
+         cell.lblBillAmount.text = String(format:"$%.2f", billList.billCalculate())
+         cell.backgroundColor = UIColor(red:0.95, green:1.00, blue:0.77, alpha:1.0)
          }
            
-        if billList.billId.contains("IN")
+        if billList.billId.contains("in")
         {
-         cell.lblbillId?.text = billList.billId
-         cell.lblbillDate?.text = billList.billDate.getFormattedDate()
-         cell.imgBillicon.image = UIImage(named:"interneticon")
-         cell.billAmount.text = String(format:"$%.2f", billList.billCalculate())
-         cell.backgroundColor = UIColor(displayP3Red: 0.9, green: 0.9, blue: 0.98, alpha: 1.0)
+         cell.lblBillId?.text = billList.billId
+         cell.lblBillDate?.text = billList.billDate.getFormattedDate()
+         cell.imgIcon.image = UIImage(named:"internetIcon")
+         cell.lblBillAmount.text = String(format:"$%.2f", billList.billCalculate())
+         cell.backgroundColor = UIColor(red:0.66, green:0.98, blue:1.00, alpha:1.0)
          }
           return cell
     }
         func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
         {
-            return CGFloat(150.0)
+            return CGFloat(100.0)
         }
  }
 
